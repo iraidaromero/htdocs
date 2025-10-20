1f76209cc4f52bf783e1f89061d04d2405ab04ef
IraidaRomeroConde <iraidaromero18@gmail.com>
2025-10-20 19:39:57 +0200

    cambios

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,20 @@
 <?php
 
-use App\Models\Hermandad;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CentroController;
-use App\Http\Controllers\HermandadController;
-use App\Http\Controllers\Hermandad_BandaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get("/mesa", [CentroController::class, "index"])->name("datos");
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get("/bandas", [Hermandad_BandaController::class, "bandasPorHermandad"]);
-Route::get("/prueba", [HermandadController::class, "index"]);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::post("/hermandad", [HermandadController::class, "create"])->name("hermandad_crear");
-Route::put("/hermandad", [HermandadController::class, "edit"])->name("hermandad_editar");
-Route::put("/hermandad/actualizar", [HermandadController::class, "update"])->name("hermandad_actualizar");
-Route::delete("/hermandad", [HermandadController::class, "destroy"])->name("hermandad_borrar");
-Route::get("/hermandad", [HermandadController::class, "index"])->name("hermandad_mostrar");
-
+require __DIR__.'/auth.php';
