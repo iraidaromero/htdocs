d586c14bbfb12c46686903ae7199293c0620e670
IraidaRomeroConde <iraidaromero18@gmail.com>
2025-10-20 16:57:47 +0200

    permitirHTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(191);
+        if($this->app->environment('production')) {
+        URL::forceScheme('https');
+        }
     }
 }
